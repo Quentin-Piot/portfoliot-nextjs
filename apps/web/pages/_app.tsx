98debd5a52e0c145591e2525b92953b8db431bb7
Quentin <quentin@sparkmate.co>
2022-05-09 18:05:17 +0800

    config + chakraui + start homepage

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {ChakraProvider, extendTheme} from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+
+  const theme = extendTheme({
+    colors: {
+      primary: "#ffffff",
+      secondary: "#eeeeee"
+    },
+  })
+  return <ChakraProvider theme={theme}> <Component {...pageProps} /></ChakraProvider>
 }
 
 export default MyApp
