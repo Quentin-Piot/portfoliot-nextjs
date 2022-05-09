0050d54331b5a722dbaa712b1c74d59f9face40d
Quentin <quentin@sparkmate.co>
2022-05-09 18:20:51 +0800

    fix vercel

--- apps/web/components/Navbars/MobileNavbar.tsx
+++ apps/web/components/Navbars/MobileNavbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import {
   Avatar,
   Box,
@@ -14,55 +14,48 @@ import {
   Text,
   useColorModeValue,
   VStack,
-} from '@chakra-ui/react';
-import {FiBell, FiChevronDown, FiMenu,} from 'react-icons/fi';
-
+} from '@chakra-ui/react'
+import { FiBell, FiChevronDown, FiMenu } from 'react-icons/fi'
 
 interface MobileProps extends FlexProps {
-  onOpen: () => void;
+  onOpen: () => void
 }
 
-const MobileNavbar = ({onOpen, ...rest}: MobileProps) => {
+const MobileNavbar = ({ onOpen, ...rest }: MobileProps) => {
   return (
     <Flex
-      ml={{base: 0, md: 60}}
-      px={{base: 4, md: 4}}
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 4 }}
       height="20"
       alignItems="center"
       bg={useColorModeValue('white', 'gray.900')}
       borderBottomWidth="1px"
       borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-      justifyContent={{base: 'space-between', md: 'flex-end'}}
-      {...rest}>
+      justifyContent={{ base: 'space-between', md: 'flex-end' }}
+      {...rest}
+    >
       <IconButton
-        display={{base: 'flex', md: 'none'}}
+        display={{ base: 'flex', md: 'none' }}
         onClick={onOpen}
         variant="outline"
         aria-label="open menu"
-        icon={<FiMenu/>}
+        icon={<FiMenu />}
       />
 
       <Text
-        display={{base: 'flex', md: 'none'}}
+        display={{ base: 'flex', md: 'none' }}
         fontSize="2xl"
         fontFamily="monospace"
-        fontWeight="bold">
+        fontWeight="bold"
+      >
         Logo
       </Text>
 
-      <HStack spacing={{base: '0', md: '6'}}>
-        <IconButton
-          size="lg"
-          variant="ghost"
-          aria-label="open menu"
-          icon={<FiBell/>}
-        />
+      <HStack spacing={{ base: '0', md: '6' }}>
+        <IconButton size="lg" variant="ghost" aria-label="open menu" icon={<FiBell />} />
         <Flex alignItems={'center'}>
           <Menu>
-            <MenuButton
-              py={2}
-              transition="all 0.3s"
-              _focus={{boxShadow: 'none'}}>
+            <MenuButton py={2} transition="all 0.3s" _focus={{ boxShadow: 'none' }}>
               <HStack>
                 <Avatar
                   size={'sm'}
@@ -71,34 +64,36 @@ const MobileNavbar = ({onOpen, ...rest}: MobileProps) => {
                   }
                 />
                 <VStack
-                  display={{base: 'none', md: 'flex'}}
+                  display={{ base: 'none', md: 'flex' }}
                   alignItems="flex-start"
                   spacing="1px"
-                  ml="2">
-                  <Text fontSize="sm">Justina Clark</Text>
+                  ml="2"
+                >
+                  <Text fontSize="sm">Quentin Piot</Text>
                   <Text fontSize="xs" color="gray.600">
                     Admin
                   </Text>
                 </VStack>
-                <Box display={{base: 'none', md: 'flex'}}>
-                  <FiChevronDown/>
+                <Box display={{ base: 'none', md: 'flex' }}>
+                  <FiChevronDown />
                 </Box>
               </HStack>
             </MenuButton>
             <MenuList
               bg={useColorModeValue('white', 'gray.900')}
-              borderColor={useColorModeValue('gray.200', 'gray.700')}>
+              borderColor={useColorModeValue('gray.200', 'gray.700')}
+            >
               <MenuItem>Profile</MenuItem>
               <MenuItem>Settings</MenuItem>
               <MenuItem>Billing</MenuItem>
-              <MenuDivider/>
+              <MenuDivider />
               <MenuItem>Sign out</MenuItem>
             </MenuList>
           </Menu>
         </Flex>
       </HStack>
     </Flex>
-  );
-};
+  )
+}
 
 export default MobileNavbar
