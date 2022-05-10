6885cc978048c1911be0d3283cfe07bdf0ce856c
Quentin <quentin@sparkmate.co>
2022-05-11 00:00:28 +0800

    meta description

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -5,7 +5,13 @@ import { theme } from './_app'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Portfolio of Quentin Piot. French software Engineer based in Asia. Well-versed in a wide range of frontend technologies (React, Vue, Next.js, Redux) and backend technologies (Node.js/Nest.js/Hapi.js, Java/Spring, GraphQl, PostgresSQL)"
+        />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
