471fd66f2b3dd46e4cc203482e49686814eab6e7
Quentin <quentin@sparkmate.co>
2022-05-22 15:59:32 +0800

    add cv to homepahe

---
+++ apps/web/components/pdfViewer/PdfViewer.tsx
@@ -0,0 +1,81 @@
+import React, { LegacyRef, useEffect, useRef, useState } from 'react'
+
+import { Document, Page, pdfjs } from 'react-pdf'
+import { Box, Button, HStack, Text, VStack } from '@chakra-ui/react'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import throttle from 'lodash/throttle'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
+const PdfViewer = ({ url = '' }) => {
+  const [numPages, setNumPages] = useState(0)
+  const [pageNumber, setPageNumber] = useState(1) //setting 1 to show fisrt page
+  const pdfWrapper = useRef<LegacyRef<HTMLDivElement>>(null)
+  const [initialWidth, setInitialWidth] = useState(1000)
+
+  const setPdfSize = () => {
+    if (pdfWrapper && pdfWrapper.current) {
+      setInitialWidth(pdfWrapper.current?.getBoundingClientRect().width)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', throttle(setPdfSize, 3000))
+    setPdfSize()
+    return () => {
+      window.removeEventListener('resize', throttle(setPdfSize, 3000))
+    }
+  }, [])
+
+  const onDocumentLoadSuccess = ({ numPages = 0 }) => {
+    setNumPages(numPages)
+    setPageNumber(1)
+  }
+
+  const changePage = (offset: number) => {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset)
+  }
+
+  const previousPage = () => {
+    changePage(-1)
+  }
+
+  const nextPage = () => {
+    changePage(1)
+  }
+
+  return (
+    <VStack width="100%">
+      <Box ref={pdfWrapper} width="100%" maxW={1000}>
+        <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} width={initialWidth} renderMode="svg" />
+        </Document>
+      </Box>
+      <Text fontWeight={600}>
+        Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+      </Text>
+      <HStack>
+        <Button
+          type="button"
+          disabled={pageNumber <= 1}
+          onClick={previousPage}
+          mr={5}
+          leftIcon={<FiChevronLeft />}
+        >
+          Previous
+        </Button>
+        <Button
+          type="button"
+          disabled={pageNumber >= numPages}
+          onClick={nextPage}
+          ml={5}
+          rightIcon={<FiChevronRight />}
+        >
+          Next
+        </Button>
+      </HStack>
+    </VStack>
+  )
+}
+
+export default PdfViewer
--- apps/web/views/Homepage.tsx
+++ apps/web/views/Homepage.tsx
@@ -1,30 +1,36 @@
 import React from 'react'
-import { Box, Center, HStack, Image, Stack, VStack } from '@chakra-ui/react'
-import BasicCard from 'components/cards/BasicCard'
-import { useTranslation } from 'react-i18next'
-import ReactMarkdown from 'react-markdown'
+import { Box, Center, Image, VStack } from '@chakra-ui/react'
+import dynamic from 'next/dynamic'
+
+const PDFViewer = dynamic(() => import('components/pdfViewer/PdfViewer'), {
+  ssr: false,
+})
 
 const Homepage: React.FC = () => {
-  const { t } = useTranslation()
   return (
     <VStack>
-      <HStack minHeight={200} width="100%" flexDirection={{ base: 'column', md: 'row' }} mb={30}>
-        <Center flex={1} height={200} pb={6}>
-          <Box>
-            <Image src="/images/me.jpg" borderRadius="full" height={200} alt={'photo of me'} />
-          </Box>
-        </Center>
-        <Stack flex={2} height="100%">
-          <BasicCard
-            categorie={t('common.aboutMe')}
-            subtitle={t('pages.homepage.cards.aboutMe.title')}
-            width="800px"
-          >
-            <br />
-            <ReactMarkdown>{t('pages.homepage.cards.aboutMe.text')}</ReactMarkdown>
-          </BasicCard>
-        </Stack>
-      </HStack>
+      {/*<HStack minHeight={200} width="100%" flexDirection={{ base: 'column', md: 'row' }} mb={30}>*/}
+      <Center flex={1} height={{ base: 90, md: 200 }} pb={6}>
+        <Image
+          src="/images/me.jpg"
+          borderRadius="full"
+          height={{ base: 90, md: 200 }}
+          alt={'photo of me'}
+        />
+      </Center>
+      {/*  <Stack flex={2} height="100%">*/}
+      {/*    <BasicCard*/}
+      {/*      categorie={t('common.aboutMe')}*/}
+      {/*      subtitle={t('pages.homepage.cards.aboutMe.title')}*/}
+      {/*      width="800px"*/}
+      {/*    >*/}
+      {/*      <br />*/}
+      {/*      <ReactMarkdown>{t('pages.homepage.cards.aboutMe.text')}</ReactMarkdown>*/}
+      {/*    </BasicCard>*/}
+      {/*  </Stack>*/}
+      {/*</HStack>*/}
+      {/*<Divider />*/}
+      <PDFViewer url="/CV_2022.pdf" />
     </VStack>
   )
 }
